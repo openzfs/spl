04613744ac053d31e575c4675918d450e26c55c5
Massimo Maggi <massimo@mmmm.it>
2012-03-18 02:21:29 +0100

    Implemented some secpolicy functions respecting Linux capabilities.
    Still not implemented:
    	secpolicy_fs_unmount because it is not used by actual ZFS code.
    	secpolicy_vnode_setids_setgids because it breaks building ZFS code as-is.
    
    Due to compiler optimizations, implementing secpolicy_vnode_setattr exposes a (not alarming) warning in ZFS code.
    
    Signed-off-by: Massimo Maggi <massimo@mmmm.it>

--- include/sys/policy.h
+++ include/sys/policy.h
@@ -21,27 +21,32 @@
  *  You should have received a copy of the GNU General Public License along
  *  with the SPL.  If not, see <http://www.gnu.org/licenses/>.
 \*****************************************************************************/
-
+#include <sys/types.h>
+#include "cred.h"
+#include "vnode.h"
 #ifndef _SPL_POLICY_H
 #define _SPL_POLICY_H
 
 #define	secpolicy_fs_unmount(c,vfs)			(0)
-#define	secpolicy_nfs(c)				(0)
-#define	secpolicy_sys_config(c,co)			(0)
-#define	secpolicy_zfs(c)				(0)
-#define	secpolicy_zinject(c)				(0)
 #define	secpolicy_vnode_setids_setgids(c,id)		(0)
-#define	secpolicy_vnode_setid_retain(c, sr)		(0)
-#define	secpolicy_setid_clear(v, c)			(0)
-#define	secpolicy_vnode_any_access(c,vp,o)		(0)
-#define	secpolicy_vnode_access2(c,cp,o,m1,m2)		(0)
-#define	secpolicy_vnode_chown(c,o)			(0)
-#define	secpolicy_vnode_setdac(c,o)			(0)
-#define	secpolicy_vnode_remove(c)			(0)
-#define	secpolicy_vnode_setattr(c,v,a,o,f,func,n)	(0)
-#define	secpolicy_xvattr(x, o, c, t)			(0)
-#define	secpolicy_vnode_stky_modify(c)			(0)
-#define	secpolicy_setid_setsticky_clear(v,a,o,c)	(0)
-#define	secpolicy_basic_link(c)				(0)
+boolean_t secpolicy_sys_config(cred_t* c,boolean_t checkonly);
+boolean_t secpolicy_nfs(cred_t* c);
+boolean_t secpolicy_zfs(cred_t* c);
+boolean_t secpolicy_zinject(cred_t* c);
+boolean_t secpolicy_vnode_setid_retain(cred_t* c,boolean_t is_setuid_root);
+boolean_t secpolicy_setid_clear(vattr_t* v,cred_t* c);
+boolean_t secpolicy_vnode_any_access(cred_t* c ,struct inode* ip,uid_t owner);
+boolean_t secpolicy_vnode_access2(cred_t* c,struct inode* ip,uid_t owner,mode_t mode1,mode_t mode2);
+boolean_t secpolicy_vnode_chown(cred_t* c,uid_t owner);
+boolean_t secpolicy_vnode_setdac(cred_t* c,uid_t owner);
+boolean_t secpolicy_vnode_remove(cred_t* c);
+boolean_t secpolicy_vnode_setattr(cred_t* c, struct inode* ip,vattr_t* vap,vattr_t* oldvap,int flags,int (*fp)(void *, int, cred_t *),void* znode); //znode_t is defined in zfs not in spl
+//this void* should be xvattr_t*, but it is defined in zfs not in spl
+boolean_t secpolicy_xvattr(void* xvattr_t,uid_t owner,cred_t* cred,mode_t mode);
+boolean_t secpolicy_vnode_stky_modify(cred_t* c);
+boolean_t secpolicy_setid_setsticky_clear(struct inode* ip,vattr_t* attr,vattr_t* oldattr,cred_t* c);
+boolean_t secpolicy_basic_link(cred_t* c);
+boolean_t secpolicy_vnode_create_gid(cred_t* c); //previously not needed
+
 
 #endif /* SPL_POLICY_H */

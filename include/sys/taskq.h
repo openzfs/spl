2eae98f1cee1ecbb02873f070a0bf65a0c6a58fe
Brian Behlendorf <behlendorf1@llnl.gov>
2015-06-03 17:21:16 -0700

    Add TASKQ_DYNAMIC feature
    Setting the TASKQ_DYNAMIC flag will create a taskq with dynamic
    semantics.  Initially only a single worker thread will be created
    to service tasks dispatched to the queue.  As additional threads
    are needed they will be dynamically spawned up to the max number
    specified by 'nthreads'.  When the threads are no longer needed,
    because the taskq is empty, they will automatically terminate.
    
    Due to the low cost of creating and destroying threads under Linux
    by default new threads and spawned and terminated aggressively.
    There are two modules options which can be tuned to adjust this
    behavior if needed.
    
    * spl_taskq_thread_sequential - The number of sequential tasks,
    without interruption, which needed to be handled by a worker
    thread before a new worker thread is spawned.  Default 1.
    
    * spl_taskq_thread_dynamic - Provides the ability to completely
    disable the use of dynamic taskqs on the system.  This is provided
    for the purposes of debugging and troubleshooting.  Default 1
    (enabled).
    
    This behavior is consistent with the dynamic taskq implemenation
    found in both illumos and FreeBSD.
    
    Signed-off-by: Brian Behlendorf <behlendorf1@llnl.gov>

--- include/sys/taskq.h
+++ include/sys/taskq.h
@@ -61,11 +61,12 @@ typedef void (task_func_t)(void *);
 typedef struct taskq {
 	spinlock_t		tq_lock;       /* protects taskq_t */
 	unsigned long		tq_lock_flags; /* interrupt state */
-	const char		*tq_name;      /* taskq name */
+	char			*tq_name;      /* taskq name */
 	struct list_head	tq_thread_list;/* list of all threads */
 	struct list_head	tq_active_list;/* list of active threads */
 	int			tq_nactive;    /* # of active threads */
-	int			tq_nthreads;   /* # of total threads */
+	int			tq_nthreads;   /* # of existing threads */
+	int			tq_maxthreads; /* max # of threads */
 	int			tq_pri;        /* priority */
 	int			tq_minalloc;   /* min task_t pool size */
 	int			tq_maxalloc;   /* max task_t pool size */
